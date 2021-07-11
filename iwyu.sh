287ecb1ca51cf63d04584c6be5c8b2bc2d38eb3e
Akuli <akuviljanen17@gmail.com>
2021-07-12 00:16:49 +0300

    aaaa

--- iwyu.sh
+++ iwyu.sh
@@ -7,6 +7,8 @@ FLAGS="$FLAGS -Xiwyu --no_fwd_decls"
 # Hide stuff that C compilers warn about anyway
 FLAGS="$FLAGS -Wno-static-local-in-inline -Wno-absolute-value"
 
+echo "IWYU flags: $FLAGS"
+
 for file in src/*.c src/*.h; do
 	# My glob thing is too dynamic or something, idk
 	if [ $file == src/glob.c ] || [ $file == src/glob.h ]; then
