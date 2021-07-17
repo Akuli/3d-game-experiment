06e61756c1eadfb381e7d9b8560b32da9e7781f1
Akuli <akuviljanen17@gmail.com>
2021-07-17 17:28:28 +0300

    switch to xz only points in place.h

--- src/place.h
+++ src/place.h
@@ -5,18 +5,18 @@
 #include "wall.h"
 #include "mathstuff.h"
 
+struct PlaceCoords { int x, z; };
+
 struct Place {
 	char path[1024];
 	struct Wall walls[MAX_WALLS];
 	int nwalls;
 	int xsize, zsize;    // players and enemies must have 0 <= x <= xsize, 0 <= z <= zsize
 
-	// y coords of locations are set to 0
-	Vec3 enemyloc;       // initial position of enemies appearing with timeout
-	Vec3 playerlocs[2];  // player initial positions
+	struct PlaceCoords enemyloc, playerlocs[2];   // initial places
 
 	// locations for enemies with neverdie set to true, created when playing begins
-	Vec3 neverdielocs[MAX_ENEMIES];
+	struct PlaceCoords neverdielocs[MAX_ENEMIES];
 	int nneverdielocs;
 };
 
