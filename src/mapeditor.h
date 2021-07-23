9032171f5e43c8e2d3e87a1abd48183d1f2f958a
Akuli <akuviljanen17@gmail.com>
2021-07-23 21:09:15 +0300

    Update src/mapeditor.h

--- src/mapeditor.h
+++ src/mapeditor.h
@@ -21,7 +21,6 @@ void mapeditor_reinit(struct MapEditor *ed, struct Map *maps, int *nmaps, int ma
 // Useful if you want to use MapEditor to display a map, but not actually edit it
 void mapeditor_displayonly_eachframe(struct MapEditor *ed);
 
-// If you want to change the map, you need to make a new map editor
 void mapeditor_setplayers(struct MapEditor *ed, const struct EllipsoidPic *plr0pic, const struct EllipsoidPic *plr1pic);
 
 // Surface passed to mapeditor_new() must be SDL_GetWindowSurface(wnd)
