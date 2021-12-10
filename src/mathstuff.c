db301ce32b504a51b6348221470669f249af1a8c
Akuli <akuviljanen17@gmail.com>
2021-12-10 21:20:25 +0200

    finish intersecting tetragons

--- src/mathstuff.c
+++ src/mathstuff.c
@@ -265,28 +265,56 @@ bool intersect_line_segments(Vec2 start1, Vec2 end1, Vec2 start2, Vec2 end2, Vec
 	return true;
 }
 
+static bool which_side_of_line(Vec2 start, Vec2 end, Vec2 point)
+{
+	Vec2 diff = vec2_sub(end, start);
+	Vec2 v = vec2_sub(point, start);
+	return mat2_det((Mat2){ .rows={
+		{ diff.x, v.x },
+		{ diff.y, v.y },
+	}}) >= 0;
+}
+
+static bool ngon_contains_point(const Vec2 *corners, int n, const Vec2 point)
+{
+	for (int i = 0; i < n; i++) {
+		Vec2 start = corners[(i+1) % n];
+		Vec2 end = corners[(i+2) % n];
+		if (which_side_of_line(start, end, point) != which_side_of_line(start, end, corners[i]))
+			return false;
+	}
+	return true;
+}
+
+// assumes convexity of outer n-gon
+static bool ngon_contains_ngon(const Vec2 *outer, const Vec2 *inner, int n)
+{
+	for (int i = 0; i < n; i++)
+		if (!ngon_contains_point(outer, n, inner[i]))
+			return false;
+	return true;
+}
+
 bool triangle_contains_point(Vec2 A, Vec2 B, Vec2 C, Vec2 point)
 {
-	/*
-	A triangle consists of the convex combination of its corners:
+	return ngon_contains_point((Vec2[]){A,B,C}, 3, point);
+}
 
-		point = tA + uB + (1-t-u)C,  where t,u >= 0 and t+u <= 1
+bool intersect_tetragons(const Vec2 *tetra1, const Vec2 *tetra2, Vec2 *ipoint)
+{
+	for (int i = 0; i < 4; i++)
+		for (int k = 0; k < 4; k++)
+			if (intersect_line_segments(tetra1[i], tetra1[(i+1)%4], tetra2[k], tetra2[(k+1)%4], ipoint))
+				return true;
 
-	Solving t and u gives a linear equation:
-		 _                _   _ _
-		| A.x-C.x  B.x-C.x | | t |
-		|                  | |   |  = point-C
-		|_A.y-C.y  B.x-C.y_| |_u_|
-	*/
-	Mat2 M = { .rows = {
-		{ A.x-C.x, B.x-C.x },
-		{ A.y-C.y, B.y-C.y },
-	}};
-	if (fabsf(mat2_det(M)) < 1e-5f)
-		return false;   // flat triangle contains nothing, lol
-
-	Vec2 tu = mat2_mul_vec2(mat2_inverse(M), vec2_sub(point, C));
-	float t = tu.x;
-	float u = tu.y;
-	return t >= 0 && u >= 0 && t+u <= 1;
+	// one tetragon can be nested inside the other
+	if (ngon_contains_ngon(tetra1, tetra2, 4)) {
+		*ipoint = tetra2[0];
+		return true;
+	}
+	if (ngon_contains_ngon(tetra2, tetra1, 4)) {
+		*ipoint = tetra1[0];
+		return true;
+	}
+	return false;
 }
--- src/mathstuff.h
+++ src/mathstuff.h
@@ -136,4 +136,10 @@ bool intersect_line_segments(Vec2 start1, Vec2 end1, Vec2 start2, Vec2 end2, Vec
 
 bool triangle_contains_point(Vec2 A, Vec2 B, Vec2 C, Vec2 point);
 
+// Both tetragons (polygon with 4 corners) should be arrays of 4 elements.
+// If returns true, sets ipoint to an example intersection point.
+// It can be, and in fact always is, on the boundary of at least one of the two tetragons.
+// Assumes convexity.
+bool intersect_tetragons(const Vec2 *tetra1, const Vec2 *tetra2, Vec2 *ipoint);
+
 #endif   // MATHSTUFF_H
