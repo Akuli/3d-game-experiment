07e149d3d2d589c87c26c4845b0d8cf578568791
Akuli <akuviljanen17@gmail.com>
2021-12-29 18:18:15 +0200

    rewrite ellipsoid-ellipsoid collision code in a much simpler way

--- src/ellipsoidbump.c
+++ src/ellipsoidbump.c
@@ -6,239 +6,109 @@
 #include <SDL2/SDL.h>
 #include "log.h"
 #include "linalg.h"
+#include "misc.h"
 
 /*
-With 1e-7f, tests sometimes hit the max number of iterations. This didn't happen
-when I tried 1e-6f. I'm setting this to even less demanding to be on the safe side.
-*/
-#define PRECISION_REQUIREMENT 1e-5f
+Finds the maximum value of
 
-/*
-Solves the equation f(x)=0 for -1 <= x <= 1, where
+	f(x) = sqrt(A1 x^2 + B1 x + C1) + sqrt(A2 x^2 + B2 x + C2)
 
-	f(x) = (Ax + B) sqrt(Cx^2 + D) + Ex.
+on an interval [a,b].
 */
-float ellipsoid_solve_the_equation(float A, float B, float C, float D, float E)
+static float find_max_value(float A1, float B1, float C1, float A2, float B2, float C2, float a, float b)
 {
-	/*
-	This initial guessing thing is very important. I didn't have much luck without:
-	- If I squared both sides to make it a 4th degree polynomial, then squaring
-	  both sides gave me "fake solutions". This is why your math teacher wants you
-	  to be careful when you ^2 both sides.
-	- Without squaring both sides, I got sqrt(x^2 - bla) type thing which
-	  wasn't defined for all x. Sometimes Newton's method wanted to suggest a value
-	  outside the interval where it was defined.
-	*/
+	SDL_assert(a <= b);
 
-	float xguess = 0;   // should be never actually used, makes compiler happy
-	float absmin = HUGE_VALF;
+	// https://en.wikipedia.org/wiki/Golden-section_search
+	// a < t < u < b (if a=b, loop below does nothing)
+	float phi = 1.618033988749895f;
+	float t = a + (2-phi)*(b-a);
+	float u = a + (-1+phi)*(b-a);
 
-	float guessingstep = 0.1f;
-	for (float x = -1; x <= 1; x += guessingstep) {
-		float f = (A*x + B)*sqrtf(C*x*x + D) + E*x;
-		if(!isfinite(f)) {
-			log_printf("got f(x) = %.10f. Arguments: A=%.10f B=%.10f C=%.10f D=%.10f E=%.10f",
-				f, A, B, C, D, E);
-			continue;
-		}
+#define f(x) (sqrtf(A1*x*x + B1*x + C1) + sqrtf(A2*x*x + B2*x + C2))
+	float ft = f(t);
+	float fu = f(u);
 
-		float absval = fabsf(f);
-		if (absval < absmin) {
-			absmin = absval;
-			xguess = x;
-		}
-	}
-
-	/*
-	Now newton's method to refine our guess quickly. Works well most of the time,
-	but some special cases occur if you jump a lot with both players by smashing
-	buttons on keyboard.
-	*/
-	float x = xguess;
-	int iter = 0;
-	float sub;
-	do {
-
-#define LOG(MSG) \
-	log_printf("%s (iter=%d x=%.10f xguess=%.10f A=%.10f B=%.10f C=%.10f D=%.10f E=%.10f)", \
-	(MSG), iter, x, xguess, A, B, C, D, E)
-
-		/*
-		Could use a squared-both-sides version of the equation here to avoid
-		calculating square roots, but this works quickly enough
-		*/
-		float sqrtstuff = sqrtf(C*x*x + D);
-		float sqrtstuff_derivative = C*x/sqrtstuff;
-		float f = (A*x + B)*sqrtstuff + E*x;
-		float f_derivative = A*sqrtstuff + (A*x + B)*sqrtstuff_derivative + E;
-		if (f_derivative == 0) {
-			LOG("derivative is zero");
-			return xguess;
-		}
-
-		// newton's method formula from high school: x_(n+1) = x_n - f(x_n)/f'(x_n)
-		// this doesn't behave nicely if derivative is zero or small
-		sub = f / f_derivative;
-		if (!isfinite(sub)) {
-			LOG("division by nonzero derivative gave something weird");
-			return xguess;
-		}
-		x -= sub;
-
-		if (fabs(x - xguess) > 2*guessingstep) {
-			LOG("x value is far away from guess");
-			return xguess;
-		}
-		if (fabsf(x) > 1) {
-			if (fabsf(x) > 1.01)
-				LOG("x value not between -1 and 1");
-			return xguess;
-		}
-
-		// it typically takes 3 or 4 iterations to make it precise, so this limit is plenty
-		if (++iter == 20) {
-			LOG("hitting max number of iterations");
+	int iter = 0;  // just in case of nan weirdness, lol
+	while (b-a > 1e-3f) {
+		if (iter++ == 25) {
+			log_printf(
+				"hitting max number of iterations: "
+				"f(x) = sqrt(%fx^2 + %fx + %f) + sqrt(%fx^2 + %fx + %f) on [%f,%f]",
+				A1,B1,C1,A2,B2,C2,a,b);
 			break;
 		}
-#undef LOG
-	} while (fabsf(sub) > PRECISION_REQUIREMENT);
-
-	return x;
-}
-
-/*
-Find the x coordinates of the points that are distance 1 away from the
-origin-centered ellipse
-
-	(x/a)^2 + (y/b)^2 = 1
-
-given the y coordinate of the points. This returns false if there are no points.
-When this returns true, you always get *pointx1 <= 0 and *pointx2 >= 0.
-*/
-bool ellipsoid_origin_centered_ellipse_distance1_points_with_given_y(
-	float a, float b, float pointy, float *pointx1, float *pointx2)
-{
-	SDL_assert(a > 0);
-	SDL_assert(b > 0);
-
-	// Ensure that solution exists: b+1 is how far up or down from x axis the points can be
-	if (fabsf(pointy) > b+1)
-		return false;
-
-	/*
-	We parametrize the ellipse as (x,y) = E(t), where
-
-		E(t) = (a cos(t), b sin(t)).
-
-	As t goes from 0 to 2pi, this point goes around the ellipse counter-clockwise.
-	With a=b=1, this is the high-school unit circle, and here a and b are just
-	stretching that.
-
-	We measure the distance from ellipse to (pointx,pointy) perpendicularly to the
-	ellipse. A vector going counter-clockwise along the ellipse is given by
-
-		E'(t) = (-a sin(t), b cos(t)).
-
-	Consider the rotation
-
-		rotate90clockwise(x, y) = (y, -x).
-
-	The vector
-
-		A(t) = rotate90clockwise(E'(t)) = (b cos(t), a sin(t))
-
-	is pointing away from the ellipse perpendicularly. Now we want
-
-		E(t) + A(t)/|A(t)| = (pointx, pointy)
-
-	where A(t)/|A(t)| is a vector of length 1 pointing away from the ellipse.
-	Comparing y coordinates of the vectors and simplifying gives
-
-		(A sin(t) + B) sqrt(C sin^2(t) + D) + E sin(t) = 0
-
-	with the following constants:
-	*/
-	float A = -b;
-	float B = pointy;
-	float C = a*a - b*b;
-	float D = b*b;
-	float E = -a;
-	float sint = ellipsoid_solve_the_equation(A, B, C, D, E);
-
-	/*
-	Above we had an equation of vectors, and so far we used only the y coordinates
-	part of it. The x coordinate part gives
-
-		pointx = ( a + b/sqrt(a^2 sin^2(t) + b^2 cos^2(t)) ) cos(t).
-
-	We have cos(t) = +- sqrt(1 - sin^2(t)), with two choices, and hence we get two
-	different x coordinates symmetrically as expected.
-	*/
-	float costSQUARED = 1 - sint*sint;
-	*pointx2 = ( a + b/sqrtf(a*a*sint*sint + b*b*costSQUARED) )*sqrtf(costSQUARED);
-	*pointx1 = -*pointx2;
-	return true;
-}
-
-/*
-How much should the ellipse move in x direction to make it not intersect the
-origin-centered unit circle?
-*/
-float ellipsoid_2d_move_amount_x_for_origin_centered_unit_circle(
-	float a, float b, Vec2 center)
-{
-	// If 0 is between these, then the unit circle and ellipse intersect
-	float xmin, xmax;
-
-	/*
-	For calling this function, we need to shift coordinates so that ellipse is at
-	origin. The unit circle's location is (-center.x, -center.y) in those
-	coordinates.
-	*/
-	if (!ellipsoid_origin_centered_ellipse_distance1_points_with_given_y(
-			a, b, -center.y, &xmin, &xmax))
-	{
-		return 0;
+		if (ft < fu) {
+			// Increases between t and u, max can't be on [a,t]
+			a = t;
+			t = u;  // magic constants using phi chosen so that this works
+			ft = fu;
+			u = a + (-1+phi)*(b-a);
+			fu = f(u);
+		} else {
+			b = u;
+			u = t;
+			fu = ft;
+			t = a + (2-phi)*(b-a);
+			ft = f(t);
+		}
 	}
-	SDL_assert(xmin <= xmax);
-
-	// Convert back to original coordinates
-	xmin += center.x;
-	xmax += center.x;
-	SDL_assert(xmin <= xmax);
-
-	if (!( xmin < 0 && 0 < xmax ))
-		return 0;
-
-	// Ellipse should move
-	if (center.x > 0) {
-		// Ellipse should move right
-		SDL_assert(xmin < 0);
-		return fabsf(xmin);
-	} else {
-		// Ellipse should move left
-		SDL_assert(xmax > 0);
-		return xmax;
-	}
+	return (ft+fu)/2;
+#undef f
 }
 
 /*
-How much to move unit circle and a horizontal line apart to make them not intersect?
-Line goes from (center.x, center.y - halflen) to (center.x, center.y + halflen).
-*/
-float ellipsoid_2d_line_and_unit_circle_move_amount(Vec2 linecenter, float halflen)
-{
-	float tmp = 1 - linecenter.y*linecenter.y;
-	if (tmp < 0)  // line above/below unit circle
-		return 0;
+Calculates how much, along a horizontal line, the ellipsoids
 
-	float res = sqrtf(tmp) - fabsf(linecenter.x) + halflen;
-	if (res < 0)  // line too far left/right from unit circle
-		return 0;
-	return res;
+	((x - center1.x)/a1)^2 + ((y - center1.y)/b1)^2 = 1
+	((x - center2.x)/a2)^2 + ((y - center2.y)/b2)^2 = 1
+
+intersect, where the line is chosen to make them intersect as little as
+possible. Negative result means no intersection.
+*/
+static float max_x_intersection_between_ellipsoids(float a1, float b1, Vec2 center1, float a2, float b2, Vec2 center2)
+{
+	// [a,b] = overlapping y coordinates of the two ellipses
+	float a = max(center1.y - b1, center2.y - b2);
+	float b = min(center1.y + b1, center2.y + b2);
+	if (a > b)
+		return -1;  // no overlap in y direction
+
+	/*
+	If center1.x < center2.x, we use right edge of ellipsoid 1 and left
+	edge of ellipsoid 2. Their distance along a horizontal line at height y:
+
+		  (center1.x + sqrt(a1^2 - (a1/b1)^2 (y - center1.y)^2))
+		- (center2.x - sqrt(a2^2 - (a2/b2)^2 (y - center2.y)^2))
+
+	If center1.x > center2.x, we get a similar thing with a couple signs
+	flipped.
+	*/
+	float k1 = (a1*a1)/(b1*b1);
+	float A1 = -k1;
+	float B1 = 2*k1*center1.y;
+	float C1 = a1*a1 - k1*center1.y*center1.y;
+
+	float k2 = (a2*a2)/(b2*b2);
+	float A2 = -k2;
+	float B2 = 2*k2*center2.y;
+	float C2 = a2*a2 - k2*center2.y*center2.y;
+
+	return find_max_value(A1, B1, C1, A2, B2, C2, a, b) - fabsf(center1.x - center2.x);
 }
 
-static float ellipse_move_amount_x(
+// Special case of the previous function: b1 = 0
+// Needs to be special cased because division by zero and stuff.
+static float max_x_intersection_between_line_and_ellipsoid(float a1, Vec2 center1, float a2, float b2, Vec2 center2)
+{
+	float ydiffrelative = (center1.y - center2.y)/b2;  // y coords overlap <=> this is between -1 and +1
+	float tmp = 1 - ydiffrelative*ydiffrelative;
+	if (tmp < 0)
+		return -1;
+	return -fabsf(center1.x - center2.x) + a1 + sqrtf(a2*a2*tmp);
+}
+
+float ellipse_move_amount_x(
 	float a1, float b1, Vec2 center1, bool hidelowerhalf1,
 	float a2, float b2, Vec2 center2)
 {
@@ -254,23 +124,21 @@ static float ellipse_move_amount_x(
 	*/
 	SDL_assert(center1.y >= center2.y || fabsf(center1.y - center2.y) < 1e-5f);
 
-	// Shift and stretch coordinates so that ellipse 2 is origin-centered unit circle
-	float a1new = a1 / a2;
-	float b1new = b1 / b2;
-	Vec2 center1new = { (center1.x - center2.x)/a2, (center1.y - center2.y)/b2 };
-
 	float xdiff;
 	if (hidelowerhalf1)
-		xdiff = ellipsoid_2d_line_and_unit_circle_move_amount(center1new, a1new);
+		xdiff = max_x_intersection_between_line_and_ellipsoid(a1, center1, a2, b2, center2);
 	else
-		xdiff = ellipsoid_2d_move_amount_x_for_origin_centered_unit_circle(a1new, b1new, center1new);
-
-	// Result is difference of x coords, unaffected by shifting, but must be unstretched
-	return xdiff * a2;
+		xdiff = max_x_intersection_between_ellipsoids(a1, b1, center1, a2, b2, center2);
+	return max(xdiff, 0);
 }
 
 float ellipsoid_bump_amount(const struct Ellipsoid *el1, const struct Ellipsoid *el2)
 {
+	// optimization for common case
+	float d = max(el1->xzradius, el1->yradius) + max(el2->xzradius, el2->yradius);
+	if (vec3_lengthSQUARED(vec3_sub(el1->center, el2->center)) > d*d)
+		return 0;
+
 	if (el1->center.y < el2->center.y) {
 		const struct Ellipsoid *tmp = el1;
 		el1 = el2;
